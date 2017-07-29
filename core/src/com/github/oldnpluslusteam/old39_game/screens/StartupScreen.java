febe75bbd2c5fe0d302ef1d6bca181e3d9715403
moardm <moiseyenkoa@gmail.com>
2017-07-29 19:04:02 +0600

    Created scene1 declaration

--- core/src/com/github/oldnpluslusteam/old39_game/screens/StartupScreen.java
+++ core/src/com/github/oldnpluslusteam/old39_game/screens/StartupScreen.java
@@ -1,8 +1,17 @@
 package com.github.oldnpluslusteam.old39_game.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.github.alexeybond.partly_solid_bicycle.application.Layer;
+import com.github.alexeybond.partly_solid_bicycle.application.Screen;
 import com.github.alexeybond.partly_solid_bicycle.application.impl.DefaultScreen;
+import com.github.alexeybond.partly_solid_bicycle.application.impl.layers.GameLayerWith2DPhysicalGame;
 import com.github.alexeybond.partly_solid_bicycle.drawing.Technique;
 import com.github.alexeybond.partly_solid_bicycle.drawing.tech.EDSLTechnique;
+import com.github.alexeybond.partly_solid_bicycle.game.Game;
+import com.github.alexeybond.partly_solid_bicycle.game.declarative.GameDeclaration;
+import com.github.alexeybond.partly_solid_bicycle.game.declarative.visitor.impl.ApplyGameDeclarationVisitor;
+import com.github.alexeybond.partly_solid_bicycle.ioc.IoC;
+import com.github.alexeybond.partly_solid_bicycle.util.parts.AParts;
 
 public class StartupScreen extends DefaultScreen {
     @Override
@@ -10,8 +19,24 @@ public class StartupScreen extends DefaultScreen {
         return new EDSLTechnique() {
             @Override
             protected Runnable build() {
-                return super.build();
+                return seq(
+                        clearColor(),
+                        pass("setup-main-camera"),
+                        pass("game-debug")
+                );
             }
         };
+    }
+
+    @Override
+    protected void createLayers(AParts<Screen, Layer> layers) {
+        super.createLayers(layers);
+
+        Game game = layers.add("game", new GameLayerWith2DPhysicalGame()).game();
+        GameDeclaration gameDeclaration = IoC.resolve(
+                "load game declaration",
+                Gdx.files.internal("scenes/scene1.json"));
+        new ApplyGameDeclarationVisitor().doVisit(gameDeclaration, game);
+
     }
 }
